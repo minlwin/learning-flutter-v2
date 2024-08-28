df91e8e258bbd34448a50d881f8ee65b901a96ec
Min Lwin <lwin.zawmin@gmail.com>
2024-08-28 10:43:43 +0630

    Update Location APP

--- s01_riverpod/p03_locations/lib/main.dart
+++ s01_riverpod/p03_locations/lib/main.dart
@@ -3,40 +3,10 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:p03_locations/pages/customer.dart';
 import 'package:p03_locations/pages/district.dart';
 import 'package:p03_locations/pages/division.dart';
 import 'package:p03_locations/pages/home.dart';
 import 'package:p03_locations/pages/township.dart';
-
-final _router = GoRouter(routes: [
-  GoRoute(
-    path: "/",
-    builder: (context, state) => const HomeScreen(),
-  ),
-  GoRoute(
-    path: "/divisions",
-    builder: (context, state) => const Divisions(),
-  ),
-  GoRoute(
-    path: "/districts/:id",
-    builder: (context, state) {
-      final id = state.pathParameters["id"];
-      return Districts(id: int.parse(id!));
-    },
-  ),
-  GoRoute(
-    path: "/townships/:id",
-    builder: (context, state) {
-      final id = state.pathParameters["id"];
-      return Townships(districtId: id!);
-    },
-  ),
-  GoRoute(
-    path: "/customers",
-    builder: (context, state) => const Customers(),
-  )
-]);
 
 void main(List<String> args) {
   runApp(ProviderScope(
@@ -62,3 +32,28 @@ class MyObserver extends ProviderObserver {
     log("Provider $provider throws $error and trace is $stackTrace");
   }
 }
+
+final _router = GoRouter(routes: [
+  GoRoute(
+    path: "/",
+    builder: (context, state) => const HomeScreen(),
+  ),
+  GoRoute(
+    path: "/divisions",
+    builder: (context, state) => const Divisions(),
+  ),
+  GoRoute(
+    path: "/districts/:id",
+    builder: (context, state) {
+      final id = state.pathParameters["id"];
+      return Districts(id: int.parse(id!));
+    },
+  ),
+  GoRoute(
+    path: "/townships/:id",
+    builder: (context, state) {
+      final id = state.pathParameters["id"];
+      return Townships(districtId: id!);
+    },
+  ),
+]);
