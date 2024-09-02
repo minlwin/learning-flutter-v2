b44d5820874a0ae05cbae2958719c2d42a65818f
Min Lwin <lwin.zawmin@gmail.com>
2024-09-03 02:06:24 +0630

    Fix backend config for Project 4

--- spring_projects/pj04_customers/src/main/java/com/jdc/customers/utils/config/JpaSpecificConfiguration.java
+++ spring_projects/pj04_customers/src/main/java/com/jdc/customers/utils/config/JpaSpecificConfiguration.java
@@ -1,10 +1,14 @@
 package com.jdc.customers.utils.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.jdc.customers.domain.BaseRepositoryImpl;
 
-@EnableJpaRepositories(repositoryBaseClass = BaseRepositoryImpl.class)
+@Configuration
+@EnableJpaRepositories(
+		repositoryBaseClass = BaseRepositoryImpl.class, 
+		basePackages = "com.jdc.customers.domain.repo")
 public class JpaSpecificConfiguration {
 
 }
